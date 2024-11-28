ed358eaccc1c560c031b620f816754904a82e921
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2024-11-28 15:04:36 +0800

    Advanced Routing

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,32 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Users List')
-})
+router.get("/", (req, res) => {
+    res.send("Users List");
+});
 
-router.get('/new', (req, res) => {
-    res.send('User New Form')
-})
+router.post("/", (req, res) => {
+    res.send("Create User");
+});
 
-module.exports = router
+router
+    .route("/:id")
+    .get((req, res) => {
+        console.log(req.user);
+        res.send(`Get User With ID ${req.params.id}`);
+    })
+    .put((req, res) => {
+        res.send(`Update User With ID ${req.params.id}`);
+    })
+    .delete((req, res) => {
+        res.send(`Delete User With ID ${req.params.id}`);
+    });
+
+const users = [{ name: "Kyle" }, { name: "Sally" }];
+
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next();
+});
+
+module.exports = router;
