8701983f87ddefd5d73b36395fb2bb2fd31b2603
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2024-11-28 14:50:19 +0800

    rendering HTML

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+// app.get('/', (req, res) => {
+//     console.log("Here")
+//     res.download('server.js')
+//     res.status(500).json({message: "Error"})
+// })
+
 app.get('/', (req, res) => {
     console.log("Here")
-    res.download('server.js')
-    res.status(500).json({message: "Error"})
+    res.render('index', {text: "World"})
 })
 
 app.listen(3000)
