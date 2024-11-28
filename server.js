f1a33ebd20516bef5dd8dc64a3f5ebef4cc8176f
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2024-11-28 15:12:44 +0800

    Rendering Static HTML

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"))
+
 app.set('view engine', 'ejs')
-
-app.use(logger)
-app.get('/', (req, res) => {
-    console.log("Here")
-    res.render('index', {text: "World"})
-})
-
-// or
-
-app.get('/', logger, (req, res) => {
-    console.log("Here")
-    res.render('index', {text: "World"})
-})
 
 const userRouter = require("./routes/users")
 
 app.use('/users', userRouter)
-
-function logger(req, res, next) {
-    console.log(req.originalUrl);
-    next()
-}
 
 app.listen(3000)
