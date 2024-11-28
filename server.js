9212d1ed012affba35cea9129bc4f65bdb7525d3
carinotj19 <67515859+carinotj19@users.noreply.github.com>
2024-11-28 14:55:25 +0800

    Routers

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-// app.get('/', (req, res) => {
-//     console.log("Here")
-//     res.download('server.js')
-//     res.status(500).json({message: "Error"})
-// })
-
 app.get('/', (req, res) => {
     console.log("Here")
     res.render('index', {text: "World"})
 })
+
+const userRouter = require("./routes/users")
+
+app.use('/users', userRouter)
 
 app.listen(3000)
